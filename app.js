e7ad1c32de90d0eb98bf5e8591abdac5ed94262d
ShayanAbedi <abedi.shayan@unb.ca>
2019-11-23 21:57:42 -0400

    work on getting the info from api display on the page

--- app.js
+++ app.js
@@ -6,19 +6,14 @@
 // //     }
 // //   }
 
-// const API_URL = "http://api.tvmaze.com/singlesearch/shows?q=";
-// const response = await fetch(API_URL + searched);
-//   // const json = await response.json();
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
-
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
+const API_URL = "http://api.tvmaze.com/singlesearch/shows?q=";
+var getit = async () => {
+  var searched = document.getElementById("searchTerm").value;
+  const response = await fetch(API_URL + searched);
+  const json = await response.json();
+  document.getElementById("card-img").setAttribute("src", json.image.medium);
+  document.getElementById("card-title").textContent = json.name;
+  document.getElementById("card-text").textContent = json.summary;
+  document.getElementById("text-muted").textContent += json.rating.average;
+  document.getElementById("container").style.visibility = "visible";
 };
-searchBtn.addEventListener("click", expand);
-input.addEventListener("keydown", e => {
-  if (e.key == "Enter") {
-    console.log(e.returnValue);
-  }
-});
